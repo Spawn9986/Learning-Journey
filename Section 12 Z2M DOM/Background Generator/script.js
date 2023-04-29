f29c825775dd7a96a6116388ae00f4b082df7c6b
Spawn9986 <scouch9986@gmail.com>
2023-04-29 15:34:32 -0500

    got the gradient feature to work

--- Section 12 Z2M DOM/Background Generator/script.js
+++ Section 12 Z2M DOM/Background Generator/script.js
@@ -1,3 +1,13 @@
 let css = document.querySelector("h3");
-let color1 = document.getElementsByClassName("color1");
-let color2 = document.getElementsByClassName("color2");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
+function updateValue(e) {
+    body.style.background = 
+        "linear-gradient(to right, " + color1.value + " , " + color2.value + ");";
+}
+
+color1.addEventListener("input", updateValue)
+color2.addEventListener("input", updateValue)
+
