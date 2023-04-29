85dbfbffe417a0c9ef73ba8ae22b45be530bf265
Spawn9986 <scouch9986@gmail.com>
2023-04-29 15:48:08 -0500

    project complete

--- Section 12 Z2M DOM/Background Generator/script.js
+++ Section 12 Z2M DOM/Background Generator/script.js
@@ -3,11 +3,17 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 
-function updateValue(e) {
+function setGradient() {
     body.style.background = 
-        "linear-gradient(to right, " + color1.value + " , " + color2.value + ");";
+        "linear-gradient(to right, " 
+        + color1.value 
+        + " , " 
+        + color2.value + 
+        ")";
+
+       /*  css.textContent = body.style.background; */
 }
 
-color1.addEventListener("input", updateValue)
-color2.addEventListener("input", updateValue)
+color1.addEventListener("input", setGradient)
+color2.addEventListener("input", setGradient)
 
