e1bfb4f678de246f61ee30e46a80c2b4022ffc46
Spawn9986 <scouch9986@gmail.com>
2023-04-30 17:44:37 -0500

    completed exercise 3 in Adv JS

--- Archive/Section 13 Z2M Advanced JavaScript/exercises/exercise3.js
+++ Archive/Section 13 Z2M Advanced JavaScript/exercises/exercise3.js
@@ -1,14 +1,19 @@
 // change everything below to the newer Javascript!
 
 // let + const
-var a = 'test';
+/* var a = 'test';
 var b = true;
 var c = 789;
+a = 'test2'; */
+
+let a = 'test';
+let b = true;
+let c = 789;
 a = 'test2';
 
 
 // Destructuring
-var person = {
+/* var person = {
     firstName : "John",
     lastName  : "Doe",
     age       : 50,
@@ -18,11 +23,20 @@ var person = {
 var firstName = person.firstName;
 var lastName = person.lastName;
 var age = person.age;
-var eyeColor = person.eyeColor;
+var eyeColor = person.eyeColor; */
+
+const person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+let { firstName, lastName, age, eyeColor } = person;
 
 
 // Object properties
-var a = 'test';
+/* var a = 'test';
 var b = true;
 var c = 789;
 
@@ -30,27 +44,51 @@ var okObj = {
   a: a,
   b: b,
   c: c
+}; */
+
+let a = 'test';
+let b = true;
+let c = 789;
+
+let okObj = {
+  a,
+  b,
+  c
 };
 
 
 // Template strings
-var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+/* var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???"; */
+
+let message = `Hello ${firstName} have I met you before? I think we met in ${city} last summer no???`;
 
 
 // default arguments
 // default age to 10;
-function isValidAge(age) {
+/* function isValidAge(age) {
+    return age
+} */
+
+function isValidAge(age=10) {
     return age
 }
 
 // Symbol
 // Create a symbol: "This is my first Symbol"
 
+const sym1 = Symbol();
+const sym2 = Symbol("foo");
+const sym3 = Symbol("foo");
+
 // Arrow functions
-function whereAmI(username, location) {
+/* function whereAmI(username, location) {
     if (username && location) {
         return "I am not lost";
     } else {
         return "I am totally lost!";
     }
+} */
+
+let whereAmI = (username, location) => {
+    return username && location ? "I am not lost" : "I am totally lost!";
 }
