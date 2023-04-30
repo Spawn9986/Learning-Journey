6eff288296dc0d59895c15c7d2ffd96a0d1652c5
Spawn9986 <scouch9986@gmail.com>
2023-04-30 16:36:29 -0500

    finished exercise 2 for Adv JS

--- Archive/Section 13 Z2M Advanced JavaScript/exercises/exercise2.js
+++ Archive/Section 13 Z2M Advanced JavaScript/exercises/exercise2.js
@@ -1,11 +1,18 @@
 //#1 change this function into a ternary and assign it to variable called experiencePoints
-function experiencePoints() {
+/* function experiencePoints() {
     if (winBattle()) {
         return 10;
     } else {
         return 1;
     }
+} */
+
+let experiencePoints = function(winBattle) {
+    return (winBattle ? 10 : 1);
 }
+
+experiencePoints(true);
+
 
 //Using this function, answer the questions below:
 function moveCommand(direction) {
@@ -32,11 +39,34 @@ function moveCommand(direction) {
 }
 
 //#2 return value when moveCommand("forward");
-
+    // my answer: undefined
 //#3 return value when moveCommand("back");
-
+    // my answer: "you arrived home"
 //#4 return value when moveCommand("right");
-
+    // my answer: "you found a river"
 //#5 return value when moveCommand("left");
-
+    // my answer: undefined
 //BONUS: practice makes perfect. Go and write your own switch function. It takes time to get used to the syntax!
+
+function avoidTornado(activity) {
+    let result;
+    switch (activity) {
+        case "fly":
+            result = "You soar above trouble but later crashed";
+            break;
+        case "drive":
+            result = "The tornado overtook you";
+            break;
+        case "run":
+            result = "You're an idiot"
+            break;
+        case "foxhole":
+            result = "Nice choice, you live"
+            break;
+        default:
+            result = "Select between fly, drive, run, or foxhole"
+    }
+    return result;
+}
+
+avoidTornado("fly");
